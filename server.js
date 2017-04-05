17e5f7fafefde48adcb7a6d40338e5d1cacf9dc4
Harshita Mehta <harshitamehta@Harshitas-MacBook-Pro.local>
2017-04-04 20:16:38 -0700

    learn to make basic requests

--- server.js
+++ server.js
@@ -21,3 +21,55 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+var router = express.Router();
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next();
+});
+
+// route middleware to validate :name
+router.param('name', function(req, res, next, name) {
+    // do validation on name here
+    // blah blah validation
+    // log something so we know its working
+    console.log('doing name validations on ' + name);
+
+    // once validation is done save the new item in the req
+    req.name = name;
+    // go to the next thing
+    next();
+});
+
+router.get('/', function(req, res) {
+  res.send('im the home page!');
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+    res.send('im the about page!');
+});
+
+// route with parameters (http://localhost:8080/hello/:name)
+router.get('/hello/:name', function(req, res) {
+    res.send('hello ' + req.params.name + '!');
+});
+
+//login
+app.route('/login')
+  .get(function(req, res) {
+    res.send('this is the login form');
+  })
+  .post(function(req,res){
+    console.log('processing');
+        res.send('processing the login form!');
+  })
+
+
+// apply the routes to our application
+app.use('/', router);
