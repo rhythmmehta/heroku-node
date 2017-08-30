1b169243c17d01c815ce6a2f5c4302ba1dbf7e12
rhythmmehta <rhythmmehta@gmail.com>
2017-08-30 12:08:36 -0700

    db connect fixed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var connString = 'postgres://atfgwsdzxlfpxs:d7a12e3fbfdcab0d7bcf4f5d06bbc4071f941ef72c2da336de19093c5ffc2040@ec2-184-73-189-221.compute-1.amazonaws.com:5432/d73qvnddija9l5';
+var connString = 'postgres://atfgwsdzxlfpxs:d7a12e3fbfdcab0d7bcf4f5d06bbc4071f941ef72c2da336de19093c5ffc2040@ec2-184-73-189-221.compute-1.amazonaws.com:5432/d73qvnddija9l5?ssl=true';
 
 var pg = require('pg');
 var express = require('express');
@@ -24,11 +24,12 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
 });
 
 var router = express.Router();
+
 
 router.get('/api/users', function(req, res, next) {
 
